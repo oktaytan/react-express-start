25ff3d2e5334acf7915aa54bfda9446686a5c879
oktaytan <oktaytanrikulu@gmail.com>
2019-04-17 16:31:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# React & Express Starter Pack
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+This app uses ''express'' npm package on server side
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run client-install`
+
+Installs user-side dependencies for this app
+
+### `npm run dev`
+
+Runs the app in the development mode.
+
+### `npm start`
+
+Runs server on port 3000
+
+### `npm run server`
+
+Runs server on port 3000 with 'modemon' in the development mode.
+
+### `npm run client`
+
+Runs the React app from the root directory
+
+### `npm run build:client`
+
+Build the React app from the root directory
---
+++ client/src/components/customers/Customers.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './Customers.css';
+
+class Customers extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      customers: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/customers')
+      .then(res => res.json())
+      .then(customers =>
+        this.setState({ customers }, () =>
+          console.log('Customers fetched...', customers)
+        )
+      );
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Customers</h2>
+        <ul>
+          {this.state.customers.map(customer => {
+            return (
+              <li key={customer.id}>
+                {customer.firstName} {customer.lastName}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Customers;
